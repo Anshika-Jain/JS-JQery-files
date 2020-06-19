85ba9b23296feb262cdc3a57b74c5707914dc040
Anshika Jain <anshikajain604@gmail.com>
2020-06-19 23:33:21 +0530

    Add files via upload

---
+++ Vanilla JavaScript Carousel/script.js
@@ -0,0 +1,7 @@
+const carouselSlide = document.querySelector('.carousel-slide');
+const carouselImages = document.querySelectorAll('.carousel-slide img');
+
+//Buttons
+const prevbtn = document.querySelector('#prevbtn');
+const nextbtn = document.querySelector('#nextbtn');
+
